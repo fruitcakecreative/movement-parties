c0451abd674b014dd8f1fa78703d38fb68784c8e
Carly Marsh <carlymarsh@carlymarsh.local>
2025-05-10 01:55:53 -0400

    update host with correct address

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
-  config.hosts << "https://new.movementparties.com"
+  config.hosts << "api-ynr1.onrender.com"
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
