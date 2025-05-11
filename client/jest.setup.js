8432a3ffb2b9326fe43463832e6a476104b0d73a
Carly Marsh <carlymarsh@carlymarsh.local>
2025-05-11 02:12:46 -0400

    fixed jest errors

--- client/jest.setup.js
+++
@@ -1,9 +0,0 @@
-const { TextEncoder, TextDecoder } = require("util");
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
-
-global.ResizeObserver = class {
-  observe() {}
-  unobserve() {}
-  disconnect() {}
-};
