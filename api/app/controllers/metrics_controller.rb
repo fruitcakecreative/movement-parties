ad470d2519d2d9e5dd5f5c102a0984f7f62964d5
Carly Marsh <carlymarsh@carlymarsh.local>
2025-05-11 17:50:06 -0400

    fixed prometheus settings

--- api/app/controllers/metrics_controller.rb
+++ api/app/controllers/metrics_controller.rb
@@ -1,5 +1,8 @@
 class MetricsController < ActionController::Base
-  http_basic_authenticate_with name: ENV["METRICS_USERNAME"], password: ENV["METRICS_PASSWORD"]
+  USERNAME = ENV["METRICS_USERNAME"] || "missing_user"
+  PASSWORD = ENV["METRICS_PASSWORD"] || "missing_pass"
+
+  http_basic_authenticate_with name: USERNAME, password: PASSWORD
 
   def index
     render plain: Yabeda::Prometheus::Exporter.export
