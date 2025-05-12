1f7f729f6b67a2ec3b97eb590cef10e2cc04b77d
Carly Marsh <carlymarsh@carlymarsh.local>
2025-05-12 13:27:50 -0400

    fixed prod.rb

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -1,4 +1,44 @@
-raise "Production booted!"
+require "active_support/core_ext/integer/time"
+
 Rails.application.configure do
+
+  config.enable_reloading = false
+
+  config.hosts << "api-ynr1.onrender.com"
+  config.hosts << "api.movementparties.com"
+
   config.eager_load = true
+
+  # Full error reports are disabled and caching is turned on.
+  config.consider_all_requests_local = false
+
+  config.active_storage.service = :local
+
+
+  config.force_ssl = true
+
+  # Log to STDOUT by default
+  config.logger = ActiveSupport::Logger.new(STDOUT)
+    .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
+    .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
+
+  # Prepend all log lines with the following tags.
+  config.log_tags = [ :request_id ]
+
+
+  config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
+
+
+  config.action_mailer.perform_caching = false
+
+  config.i18n.fallbacks = true
+
+  config.active_support.report_deprecations = false
+
+  # Do not dump schema after migrations.
+  config.active_record.dump_schema_after_migration = false
+
+  # Only use :id for inspections in production.
+  config.active_record.attributes_for_inspect = [ :id ]
+
 end
