d07a9955271d01b7e7c7bd13848efd9b3ea56f79
Carly Marsh <carlymarsh@carlymarsh.local>
2025-05-11 23:02:33 -0400

    fixed Header issue

---
+++ client/src/components/MainHeader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function MainHeader() {
+
+  return (
+  <div className="header-con">
+    <div className="logo-con"><img alt="movement parties logo" src="/images/logo_mobile.png" /></div>
+  </div>
+)};
+
+export default MainHeader;
--- client/src/layout.js
+++ client/src/layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Header from './components/Header';
+import MainHeader from './components/MainHeader';
 
 const Layout = () => (
   <>
