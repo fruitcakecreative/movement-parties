4bbdebb26f86553a07581ef265d58ea7429e15d0
Carly Marsh <carlymarsh@carlymarsh.local>
2025-05-21 17:03:17 -0400

    Update sentry setup

--- client/src/setup/sentry.js
+++ client/src/setup/sentry.js
@@ -1,7 +1,7 @@
 import * as Sentry from '@sentry/react';
 
 Sentry.init({
-  dsn: 'https://a0d55299eb711f8afc2d33676cfa2980@o4509307444264960.ingest.us.sentry.io/4509307445444608',
+  dsn: process.env.SENTRY_DSN,
   environment: process.env.REACT_APP_ENV || 'development',
   release: process.env.REACT_APP_RELEASE,
   sendDefaultPii: true,
@@ -9,7 +9,7 @@ Sentry.init({
     Sentry.browserTracingIntegration(),
     Sentry.replayIntegration()
   ],
-  replaysSessionSampleRate: 0.1,
+  replaysSessionSampleRate: 1.0,
   replaysOnErrorSampleRate: 1.0,
   tracesSampleRate: 1.0,
   tracePropagationTargets: ['localhost', 'https://stagingapi.movementparties.com', 'https://api.movementparties.com'],
