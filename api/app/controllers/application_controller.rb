1b07e0f8bd4693250a74e1df7208b02516316830
Carly Marsh <carlymarsh@carlymarsh.home.local>
2026-02-26 23:18:30 -0500

    updates

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
+# app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
   include ActionController::Cookies
   include Devise::Controllers::Helpers
 
   protect_from_forgery with: :exception
-  before_action :set_current_city_key
-
+  skip_before_action :verify_authenticity_token, if: -> { request.path.start_with?("/api") }
 end
